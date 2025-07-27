a2f799ca43667c40dbda053fedd02aea40f7967a
scripts <nielanthonyr@gmail.com>
2025-07-27 11:31:26 +0800

    script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/MImWNM1PaxrZrm3P/raw", true))()
+loadstring(game:HttpGet("https://pastefy.app/As2mBnGJ/raw"))()
 
 local Players = game:GetService("Players")
 local Workspace = game:GetService("Workspace")
